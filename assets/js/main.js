79fafc0508e5be7aea98de89adb6eeef246f2dd7
rawlinsonlima <hallyssom@gmail.com>
2024-07-02 16:48:17 -0300

    Ajuste portifólio

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,8 +12,31 @@ function updateHardSkills(profileData) {
     .join("");
 }
 
+function updateLanguages(profileData) {
+  const languages = document.getElementById("profile.languages");
+  languages.innerHTML = profileData.languages
+    .map((language) => `<li>${language}</li>`)
+    .join("");
+}
+
+function updatePortifolio(profileData) {
+  const portifolio = document.getElementById("profile.portifolio");
+  portifolio.innerHTML = profileData.portifolio
+    .map((project) => {
+      return `
+        <li>
+            <h3 ${project.github ? 'class = "github"' : ""}>${project.name}</h3>
+            <a href="${project.url}" target="_blank">${project.url}</a>
+        </li>
+        `;
+    })
+    .join("");
+}
+
 (async () => {
   const profileData = await fetchProfileData();
   updateSoftSkills(profileData);
   updateHardSkills(profileData);
+  updateLanguages(profileData);
+  updatePortifolio(profileData);
 })();
