8a8b048fb3f2baefed8e070fba791dd3ed616826
rawlinsonlima <hallyssom@gmail.com>
2024-07-02 16:02:22 -0300

    Ajuste arquivo Json link das imagens dos icones hardskills

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
+function updateSoftSkills(profileData) {
+  const softSkills = document.getElementById("profile.skills.softskills");
+  softSkills.innerHTML = profileData.skills.softSkills
+    .map((skill) => `<li>${skill}</li>`)
+    .join("");
+}
+
+function updateHardSkills(profileData) {
+  const hardSkills = document.getElementById("profile.skills.hardskills");
+  hardSkills.innerHTML = profileData.skills.hardSkills
+    .map((skill) => `<li><img src="${skill.logo}" alt="${skill.name}"></li>`)
+    .join("");
+}
+
 (async () => {
   const profileData = await fetchProfileData();
-  console.log(profileData);
+  updateSoftSkills(profileData);
+  updateHardSkills(profileData);
 })();
