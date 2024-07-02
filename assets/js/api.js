b64a90009b20c31faaa409c77348d09bfbaa20ad
rawlinsonlima <hallyssom@gmail.com>
2024-07-02 11:43:34 -0300

    Ajuste arquivo Json proprio finalizado as exp profs e trocado o link da api para o link proprio

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchProfileData() {
   const url =
-    "https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json";
+    "https://raw.githubusercontent.com/rawlinsonlima/Rawlinson-Lima-Portifolio/main/data/profile.json";
   const fetching = await fetch(url);
   return await fetching.json();
 }
